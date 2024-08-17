ce16e674d8fc90ae0de3dd1d17a9dcc31ed6c7bd
dah4k <84741290+dah4k@users.noreply.github.com>
2024-08-17 18:53:09 -0400

    Create second test
    % dotnet test
    ...
    [xUnit.net 00:00:00.13]     Tests.Sequences of Ones and Evens returns Ones [FAIL]
      Failed Tests.Sequences of Ones and Evens returns Ones [9 ms]
      Error Message:
       Assert.Equal() Failure: Collections differ
                                    ↓ (pos 0)
    Expected: FSharpList<int>      [1, 1, 1, 1]
    Actual:   EmptyEnumerable<int> []

--- MathService.Tests/Tests.fs
+++ MathService.Tests/Tests.fs
@@ -9,3 +9,9 @@ let ``Sequence of Evens returns empty collection`` () =
     let expected = Seq.empty<int>
     let actual = MyMath.squaresOfOdds [ 2; 4; 6; 8; 10 ]
     Assert.Equal<Collections.Generic.IEnumerable<int>>(expected, actual)
+
+[<Fact>]
+let ``Sequences of Ones and Evens returns Ones`` () =
+    let expected = [1; 1; 1; 1]
+    let actual = MyMath.squaresOfOdds [2; 1; 4; 1; 6; 1; 8; 1; 10]
+    Assert.Equal<Collections.Generic.IEnumerable<int>>(expected, actual)
