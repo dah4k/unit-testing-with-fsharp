86df0d89d671e1510fd58ec6cde627e243eff610
dah4k <84741290+dah4k@users.noreply.github.com>
2024-08-17 18:59:43 -0400

    Fix implementation to pass third test

--- MathService/Library.fs
+++ MathService/Library.fs
@@ -2,7 +2,9 @@ namespace MathService
 
 module MyMath =
     let private isOdd x = x % 2 <> 0
+    let private square x = x * x
 
     let squaresOfOdds xs =
         xs
         |> Seq.filter isOdd
+        |> Seq.map square
